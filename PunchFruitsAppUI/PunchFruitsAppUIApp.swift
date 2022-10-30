68f22d33b3d51d7e20cdf7985c350653f3970eba
Avijeet Pandey <avijeetpandey25@gmail.com>
2022-10-30 21:05:55 +0530

    Initial Commit

---
+++ PunchFruitsAppUI/PunchFruitsAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PunchFruitsAppUIApp.swift
+//  PunchFruitsAppUI
+//
+//  Created by Avijeet Pandey on 30/10/22.
+//
+
+import SwiftUI
+
+@main
+struct PunchFruitsAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
